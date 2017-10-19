b1b6d384efe02964fef6c0a1a58f870e058f1853
Matt Peperell <matt@peperell.com>
2017-10-19 09:22:03 +0100

    Initial version

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var path = require('path');
+var redis = require('redis-url').connect()
+
+var app = express();
+app.set('view engine', 'jade');
+
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+
+// no routes have been set up, so we'll capture the URL path and look that up in redis for the final URL
+app.use(function(req, res, next) {
+	redis.get( req.path, function (err, target) {
+    if (!target) {
+      res.status = 404
+      res.render('404', {
+        key: req.path
+      });
+      return
+    }
+
+    // optionally add a referrer to this shortner?
+    res.redirect(target)
+	})
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
